8934d3097e19f3380b1e1801cf4798225c7dd35f
Yaka <yaka88@gmail.com>
2023-08-17 15:29:44 +0800

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Salary4Zhen
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.listMonth = new System.Windows.Forms.ListBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.btnReport = new System.Windows.Forms.Button();
+            this.btnAnalysis = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnImport = new System.Windows.Forms.Button();
+            this.radioHang = new System.Windows.Forms.RadioButton();
+            this.radioHuzhou = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // listMonth
+            // 
+            this.listMonth.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listMonth.FormattingEnabled = true;
+            this.listMonth.ItemHeight = 29;
+            this.listMonth.Items.AddRange(new object[] {
+            "1月",
+            "2月",
+            "3月",
+            "4月",
+            "5月",
+            "6月",
+            "7月",
+            "8月",
+            "9月",
+            "10月",
+            "11月",
+            "12月",
+            "1月"});
+            this.listMonth.Location = new System.Drawing.Point(34, 12);
+            this.listMonth.Name = "listMonth";
+            this.listMonth.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.listMonth.Size = new System.Drawing.Size(76, 439);
+            this.listMonth.TabIndex = 0;
+            this.listMonth.SelectedValueChanged += new System.EventHandler(this.listMonth_SelectedValueChanged);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(333, 128);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(116, 34);
+            this.btnCalculate.TabIndex = 1;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(333, 326);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(116, 34);
+            this.btnExport.TabIndex = 2;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // btnReport
+            // 
+            this.btnReport.Location = new System.Drawing.Point(333, 189);
+            this.btnReport.Name = "btnReport";
+            this.btnReport.Size = new System.Drawing.Size(116, 34);
+            this.btnReport.TabIndex = 3;
+            this.btnReport.Text = "Report";
+            this.btnReport.UseVisualStyleBackColor = true;
+            this.btnReport.Click += new System.EventHandler(this.btnReport_Click);
+            // 
+            // btnAnalysis
+            // 
+            this.btnAnalysis.Location = new System.Drawing.Point(333, 258);
+            this.btnAnalysis.Name = "btnAnalysis";
+            this.btnAnalysis.Size = new System.Drawing.Size(116, 34);
+            this.btnAnalysis.TabIndex = 4;
+            this.btnAnalysis.Text = "Analysis";
+            this.btnAnalysis.UseVisualStyleBackColor = true;
+            this.btnAnalysis.Click += new System.EventHandler(this.btnAnalysis_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 60000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(333, 70);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(116, 34);
+            this.btnImport.TabIndex = 5;
+            this.btnImport.Text = "Import";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // radioHang
+            // 
+            this.radioHang.AutoSize = true;
+            this.radioHang.Checked = true;
+            this.radioHang.Location = new System.Drawing.Point(150, 104);
+            this.radioHang.Name = "radioHang";
+            this.radioHang.Size = new System.Drawing.Size(108, 24);
+            this.radioHang.TabIndex = 6;
+            this.radioHang.TabStop = true;
+            this.radioHang.Text = "Hangzhou";
+            this.radioHang.UseVisualStyleBackColor = true;
+            // 
+            // radioHuzhou
+            // 
+            this.radioHuzhou.AutoSize = true;
+            this.radioHuzhou.Location = new System.Drawing.Point(150, 158);
+            this.radioHuzhou.Name = "radioHuzhou";
+            this.radioHuzhou.Size = new System.Drawing.Size(90, 24);
+            this.radioHuzhou.TabIndex = 7;
+            this.radioHuzhou.TabStop = true;
+            this.radioHuzhou.Text = "Huzhou";
+            this.radioHuzhou.UseVisualStyleBackColor = true;
+            this.radioHuzhou.CheckedChanged += new System.EventHandler(this.listMonth_SelectedValueChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 483);
+            this.ControlBox = false;
+            this.Controls.Add(this.radioHuzhou);
+            this.Controls.Add(this.radioHang);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.btnAnalysis);
+            this.Controls.Add(this.btnReport);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.listMonth);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Salary For Zhen Only";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listMonth;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Button btnReport;
+        private System.Windows.Forms.Button btnAnalysis;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.RadioButton radioHang;
+        private System.Windows.Forms.RadioButton radioHuzhou;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Data;
+using System.Data.OleDb;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+//using System.Diagnostics;
+using Microsoft.Win32;
+using System.Runtime.InteropServices;
+
+namespace Salary4Zhen
+{
+    public partial class MainForm : Form
+    {
+        public static string strConn = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0}\\Salary.accdb";
+        public static string strConnExcel = "Provider=Microsoft.Ace.OleDb.12.0;Data Source = {0};Extended Properties='Excel 12.0;HDR=Yes'";
+        public static string strFilter = "Excel File(*.xls;*.xlsx)|*.xls;*.xlsx";
+        public static string strWeb = @"http://bluelaser.cn:88/bluelaser/Yaka.txt";
+        public bool bValid = true;
+        public string strMonth = "";
+
+        //for 整点提醒
+        public string strUserName = "Yaka";
+        public static string strMessage = "{0}用时{1:F4}秒,总计执行{2}条记录,请务必休息{3}分钟后再工作！";
+        public static string strMessageHours = "，请站起来上个厕所，喝个开水，放松眼睛，休息5分钟后再工作！";
+        public static string strMessageLunch = "，午饭时间到了，请按时吃饭，5分钟后自动锁屏！";
+        public static string strMessageOffduty = "，下班时间到了，请放下工作，回家吃饭，5分钟后自动关机！";
+        
+        //for import
+        public static string strSQLDel0 = "DELETE * FROM [{0}工资] ";
+        public static string strSQLImport = "INSERT INTO [{0}工资] SELECT * FROM [excel 12.0;database={1}].[sheet1$]";
+        //for export
+        public static string strSQLExport = "SELECT * INTO [excel 12.0;database={0}].[{1}] FROM [{1}工资]";
+
+        //for 扣回汇总
+        public static string strSQL0 = "DELETE * FROM 扣回汇总 WHERE 所属月份='{0}'";
+        public static string strSQL1 = @"INSERT INTO 扣回汇总 ( 所属月份, 骑手ID, 骑手信息, 所属团队, 在职情况, 房租, 电瓶车, 物资, 充电 )
+                                     SELECT '{0}', 骑手ID, 骑手信息, 所属团队, 在职情况, 房租, 电瓶车, 物资, 充电 
+                                     FROM {0}工资 WHERE 应发工资 >=0 AND(房租<0 OR 电瓶车< 0 OR  物资< 0 OR 充电< 0)";
+        public static string strSQL2 = @"INSERT INTO 扣回汇总 ( 所属月份, 骑手ID, 骑手信息, 所属团队, 在职情况, 房租, 电瓶车, 物资, 充电 )
+                                     SELECT '{0}', 骑手ID, 骑手信息, 所属团队, 在职情况, 房租, 电瓶车, 物资, 充电 
+                                     FROM {0}工资 WHERE 应发工资< 0 AND (房租<0 OR 电瓶车< 0 OR  物资< 0 OR 充电< 0)  
+                                     AND 骑手ID IN (SELECT 骑手ID FROM {1}工资 WHERE 上月遗留< 0 AND (应发工资 - 房租 - 电瓶车- 物资- 充电 - 预支)>= 0)";
+        public static string strSQL3 = @"INSERT INTO 扣回汇总 ( 所属月份, 骑手ID, 骑手信息, 所属团队, 在职情况, 房租, 电瓶车, 物资, 充电, 部分扣回 ) 
+                                     SELECT '{0}', 骑手ID, 骑手信息, 所属团队, 在职情况, 房租, 电瓶车, 物资, 充电, 应发工资 - 房租 - 电瓶车- 物资- 充电 - 预支 
+                                     FROM {0}工资 WHERE 应发工资< 0 AND (房租<0 OR 电瓶车< 0 OR  物资< 0 OR 充电< 0) 
+                                     AND 应发工资 - 房租 - 电瓶车- 物资- 充电 - 预支>0 AND 骑手ID NOT IN(SELECT 骑手ID FROM {1}工资)";
+        public static string strSQL4 = @"INSERT INTO 扣回汇总 ( 所属月份, 骑手ID, 骑手信息, 所属团队, 在职情况, 房租, 电瓶车, 物资, 充电, 部分扣回 ) 
+                                     SELECT '{0}', [{0}工资].骑手Id, 骑手信息, 所属团队, 在职情况, 房租, 电瓶车, 物资, 充电, 部分扣回 
+                                     FROM {0}工资 INNER JOIN(SELECT 骑手ID, (应发工资 - 房租 - 电瓶车- 物资- 充电 - 预支 - 上月遗留) AS 部分扣回 FROM {1}工资 WHERE 上月遗留< 0 AND 应发工资 - 房租 - 电瓶车- 物资- 充电 - 预支< 0)  AS Nextview  
+                                     ON [{0}工资].骑手id = Nextview.骑手id WHERE Nextview.部分扣回 > 0";
+        public static string strSQLSelect = "SELECT * FROM 扣回汇总 WHERE 部分扣回 > 0 AND 所属月份 = '{0}'";
+        public static string strSQLInto0 = "SELECT * INTO [excel 12.0;database={0}].[{1}] FROM 扣回汇总 WHERE 所属月份 = '{1}'";
+
+        //for 薪资报备
+        public static string strSQLDel = "DELETE * FROM 薪资报备";
+        public static string strSQLInto = @"INSERT INTO 薪资报备 SELECT * FROM [excel 12.0;database={0}].[sheet1$]";
+        public static string strSQL5 = @"UPDATE 薪资报备 INNER JOIN [{0}工资] ON(薪资报备.团队名称 = [{0}工资].所属团队) AND(薪资报备.骑手ID = [{0}工资].骑手Id) 
+                                    SET 装备物资 = 物资, 站点充电 = 充电, 住宿费用 = 房租, 薪资报备.保险 = [{0}工资].保险, 薪资报备.个税 = [{0}工资].个税,骑手预支借款 = -预支,骑手预支还款 = 0, 实发薪资 = 实发工资-[{0}工资].保险-房租-物资-充电-预支";
+        //public static string strSQL51 = @"UPDATE 薪资报备 INNER JOIN (SELECT * FROM {0}工资 WHERE 骑手ID not in (SELECT 骑手ID FROM {0}报备 WHERE 实发薪资 IS NOT NULL)) AS 工资View
+        //                           ON [薪资报备].骑手ID = 工资View.骑手Id SET 装备物资 = 物资, 站点充电 = 充电, 住宿费用 = 房租, [{0}报备].保险 = 工资View.保险, [{0}报备].个税 = 工资View.个税,骑手预支借款 = -预支, 骑手预支还款 = 0,实发薪资 = 实发工资-工资View.保险-房租-物资-充电-预支";
+        public static string strSQL6 = "UPDATE 薪资报备 SET 实发薪资 = 0 WHERE 实发薪资 < 0";
+        public static string strSQL7 = "UPDATE 薪资报备 SET [其他加项(金额)] = 实发薪资-(底薪+提成+价格奖励+距离奖励+时段奖励+天气奖励+好评奖励+冲单奖励+差评扣款+投诉扣款+违规扣款+商户索赔+装备物资+站点充电+住宿费用+保险+个税+骑手预支借款) WHERE 实发薪资 IS NOT NULL ";
+        public static string strSQL8 = "UPDATE 薪资报备 SET [其他加项(金额)] = 0,[其他减项(金额)] = [其他加项(金额)] WHERE [其他加项(金额)] < 0 ";
+        public static string strSQLInto2 = "SELECT * INTO [excel 12.0;database={0}].薪资报备 FROM 薪资报备";
+        
+        //for 工资计算
+        public static string strSQL9 = "UPDATE [工资表$] INNER JOIN [成立差评$] ON [成立差评$].骑手信息 = [工资表$].骑手信息 AND [成立差评$].团队 = [工资表$].团队名称 SET 成立差评 = 汇总";
+        [DllImport("user32.dll")]
+        public static extern void LockWorkStation();
+        public MainForm()
+        {
+            InitializeComponent();
+            listMonth.SetSelected(DateTime.Now.Month-2, true);
+        }
+
+        private bool CheckYaka()
+        {
+            try
+            {
+                HttpWebRequest LoginReq = (HttpWebRequest)WebRequest.Create(strWeb);
+                HttpWebResponse LoginRes = (HttpWebResponse)LoginReq.GetResponse();
+                Stream receiveStream = LoginRes.GetResponseStream();
+                StreamReader readStream = new StreamReader(receiveStream);
+                bValid = (readStream.ReadToEnd() == "Yaka");
+                readStream.Close();
+                LoginRes.Close();
+            }
+            catch (WebException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return bValid;
+        }
+        private void btnAnalysis_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = strFilter;
+            if (ofd.ShowDialog() == DialogResult.OK && bValid)
+            {
+                OleDbConnection conn = new OleDbConnection(string.Format(strConn, Application.StartupPath));
+
+                string strNextMonth = listMonth.Items[listMonth.SelectedIndex + 1].ToString();
+                if (radioHuzhou.Checked)
+                    strNextMonth = 'H' + strNextMonth;
+                try
+                {
+                    DateTime timeStart = DateTime.Now;
+                    conn.Open();
+                    OleDbCommand commandInsert = new OleDbCommand(string.Format(strSQL0, strMonth), conn);
+                    int intLow = commandInsert.ExecuteNonQuery();
+                    commandInsert.CommandText = string.Format(strSQL1, strMonth, strNextMonth);
+                    intLow = commandInsert.ExecuteNonQuery();
+                    commandInsert.CommandText = string.Format(strSQL2, strMonth, strNextMonth);
+                    intLow += commandInsert.ExecuteNonQuery();
+                    commandInsert.CommandText = string.Format(strSQL3, strMonth, strNextMonth);
+                    intLow += commandInsert.ExecuteNonQuery();
+                    commandInsert.CommandText = string.Format(strSQL4, strMonth, strNextMonth);
+                    intLow += commandInsert.ExecuteNonQuery();
+
+                    OleDbCommand command = new OleDbCommand(string.Format(strSQLSelect, strMonth), conn);
+                    OleDbDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        StringBuilder strSQL = new StringBuilder("UPDATE 扣回汇总 SET ");
+                        int intpart = (int)reader["部分扣回"];
+                        if (intpart <= -(int)reader["房租"])
+                            strSQL.Append("房租=-" + intpart.ToString() + ",电瓶车=0,物资=0,充电=0");
+                        else
+                        {
+                            intpart += (int)reader["房租"];
+                            if (intpart <= -(int)reader["电瓶车"])
+                                strSQL.Append("电瓶车=-" + intpart.ToString() + ",物资=0,充电=0");
+                            else
+                            {
+                                intpart += (int)reader["电瓶车"];
+                                if (intpart <= -(int)reader["物资"])
+                                    strSQL.Append("物资=-" + intpart.ToString() + ",充电=0");
+                                else
+                                {
+                                    intpart += (int)reader["物资"];
+                                    if (intpart <= -(int)reader["充电"])
+                                        strSQL.Append("充电=-" + intpart.ToString());
+                                    else
+                                        continue;
+                                }
+                            }
+                        }
+                        strSQL.Append(" WHERE 所属月份 = '" + strMonth + "' AND 骑手Id=" + reader["骑手Id"].ToString());
+                        OleDbCommand command2 = new OleDbCommand(strSQL.ToString(), conn);
+                        intLow += command2.ExecuteNonQuery();
+                    }
+                    reader.Close();
+
+                    commandInsert.CommandText = string.Format(strSQLInto0, ofd.FileName,strMonth);
+                    commandInsert.ExecuteNonQuery();
+                    MessageBox.Show(string.Format(strMessage, strMonth + btnAnalysis.Text, (DateTime.Now - timeStart).TotalSeconds, intLow, intLow / 10), "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                catch (OleDbException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    if (conn.State == ConnectionState.Open)
+                        conn.Close();
+                }
+            }
+        }
+
+        private void btnReport_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = strFilter;
+            if (ofd.ShowDialog() == DialogResult.OK && bValid)
+            {
+                OleDbConnection connAccess = new OleDbConnection(string.Format(strConn, Application.StartupPath));
+                try
+                {
+                    DateTime timeStart = DateTime.Now;
+                    connAccess.Open();
+                    OleDbCommand commandInsert = new OleDbCommand(strSQLDel, connAccess);
+                    int intLow = commandInsert.ExecuteNonQuery();
+                    commandInsert.CommandText = string.Format(strSQLInto, ofd.FileName);
+                    intLow = commandInsert.ExecuteNonQuery();
+                    commandInsert.CommandText = string.Format(strSQL5, strMonth);
+                    intLow = commandInsert.ExecuteNonQuery();
+                    commandInsert.CommandText = string.Format(strSQL6, strMonth);
+                    intLow = commandInsert.ExecuteNonQuery();
+                    commandInsert.CommandText = string.Format(strSQL7, strMonth);
+                    intLow = commandInsert.ExecuteNonQuery();
+                    commandInsert.CommandText = string.Format(strSQL8, strMonth);
+                    intLow = commandInsert.ExecuteNonQuery();
+                    commandInsert.CommandText = string.Format(strSQLInto2, ofd.FileName);
+                    commandInsert.ExecuteNonQuery();
+                    MessageBox.Show(string.Format(strMessage, strMonth + btnReport.Text, (DateTime.Now - timeStart).TotalSeconds, intLow, intLow / 10), "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                }
+                catch (OleDbException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    if (connAccess.State == ConnectionState.Open)
+                        connAccess.Close();
+                }
+            }
+        }
+
+        /*
+        private void save3DData()
+        {
+            string strConn = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Yaka\Documents\Note.accdb";
+            OleDbConnection connAccess = new OleDbConnection(strConn);
+            connAccess.Open();
+            OleDbCommand commandInsert = new OleDbCommand(strSQLDel, connAccess);
+
+            string strPath = @"G:\3D";
+            savefiles(strPath, commandInsert);
+            string[] strDirs = Directory.GetDirectories(strPath);
+            foreach (string strDir in strDirs)
+            {
+                savefiles(strDir, commandInsert);
+            }
+            connAccess.Close();
+        }
+        private void savefiles(string strPath, OleDbCommand commandInsert)
+        {
+            string strSQL = "INSERT INTO 3DMovies (FileName,series,ShowYear) VALUES ('{0}','{1}',{2})";
+            string[] strfiles = Directory.GetFiles(strPath, @"*.avi");
+            string strseries = strPath.Substring(strPath.LastIndexOf("\\") + 1);
+            bool is3d = strseries == "3D";
+            int intLow;
+            foreach (string strfile in strfiles)
+            {
+                string strname = strfile.Substring(strfile.LastIndexOf("\\") + 1);
+                int intindex = strname.IndexOf("20");
+                string intYear = "0";
+                if (intindex > -1)
+                    intYear = strname.Substring(intindex, 4);
+                if(is3d)
+                {
+                    strseries = strname.Substring(0, intindex-1);
+                }
+                commandInsert.CommandText = string.Format(strSQL, strname, strseries, intYear);
+                intLow = commandInsert.ExecuteNonQuery();
+            }
+        }
+        */
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("TODO");  
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(DateTime.Now.Minute == 30)
+            {
+                if (DateTime.Now.Hour == 11)
+                {
+                    MessageBox.Show(strUserName + strMessageLunch, "整点播报", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                }
+                else if (DateTime.Now.Hour == 18)
+                {
+                    MessageBox.Show(strUserName + strMessageOffduty, "整点播报", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                }
+                else
+                    MessageBox.Show(strUserName + strMessageHours, "整点播报", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (DateTime.Now.Minute == 35)
+            {
+                if (DateTime.Now.Hour == 18)
+                {
+                    // Process.Start("shutdown.exe", "-s");
+                    LockWorkStation();
+                }
+                else if (DateTime.Now.Hour == 11)
+                {
+                    LockWorkStation();
+                    //Process.Start("shutdown.exe", "-l");
+                }
+            }
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            RegistryKey R_local = Registry.CurrentUser;
+            RegistryKey R_run = R_local.CreateSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run");
+            R_run.SetValue("Salary4Zhen", Application.ExecutablePath); 
+            R_run.Close();
+            R_local.Close();
+           // if (Environment.UserName != strUserName)
+            strUserName = Environment.UserName;
+            //CheckYaka();
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = strFilter;
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                OleDbConnection conn = new OleDbConnection(string.Format(strConn, Application.StartupPath));
+                
+                try
+                {
+                    DateTime timeStart = DateTime.Now;
+                    conn.Open(); 
+                    OleDbCommand commandInsert = new OleDbCommand(string.Format(strSQLDel0, strMonth), conn);
+                    int intLow = commandInsert.ExecuteNonQuery();
+                    commandInsert.CommandText = string.Format(strSQLImport, strMonth, ofd.FileName);
+                    intLow = commandInsert.ExecuteNonQuery();
+                    MessageBox.Show(string.Format(strMessage, strMonth + btnImport.Text, (DateTime.Now - timeStart).TotalSeconds, intLow, intLow / 100), "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                catch (OleDbException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    if (conn.State == ConnectionState.Open)
+                        conn.Close();
+                }
+            }
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = strFilter;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                OleDbConnection conn = new OleDbConnection(string.Format(strConn, Application.StartupPath));
+                try
+                {
+                    DateTime timeStart = DateTime.Now;
+                    conn.Open();
+                    OleDbCommand commandInsert = new OleDbCommand(string.Format(strSQLExport, ofd.FileName, strMonth), conn);
+                    int intLow = commandInsert.ExecuteNonQuery();
+                    MessageBox.Show(string.Format(strMessage, strMonth + btnExport.Text, (DateTime.Now - timeStart).TotalSeconds, intLow, intLow / 100), "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                catch (OleDbException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                finally
+                {
+                    if (conn.State == ConnectionState.Open)
+                        conn.Close();
+                }
+
+            }
+        }
+
+        private void listMonth_SelectedValueChanged(object sender, EventArgs e)
+        {
+            strMonth = (radioHuzhou.Checked ? 'H' + listMonth.Text : listMonth.Text);
+        }
+
+      
+    }
+}
